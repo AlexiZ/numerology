94c3262bc03fd80c14dd496f29ef06741b025cf6
Alexis <alexis.mazaudier@lephare.com>
2019-07-01 17:51:21 +0200

    Moved single current capistrano env as preprod one, live server is coming soon

--- app/config/capistrano/stages/preprod.rb
+++ app/config/capistrano/stages/preprod.rb
@@ -1,10 +1,10 @@
-set :stage, :production
+set :stage, :preprod
 set :domain, 'home655277423.1and1-data.host'
 
 server fetch(:domain), user: 'u87073604', roles: %w{web app db}
 
 set :deploy_to, '/kunden/homepages/39/d655277423/htdocs/numerology'
-set :branch, 'master'
+set :branch, 'develop'
 
 set :composer_install_flags, '--no-dev --prefer-dist --no-interaction --quiet --optimize-autoloader --ignore-platform-reqs'
 
